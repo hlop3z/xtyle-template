721f21d8678214569f98501654f60f3108c85b8b
hlop3z <23062270+hlop3z@users.noreply.github.com>
2024-05-30 17:12:35 -0500

    Update demo.tsx

--- src/preview/demo.tsx
+++ src/preview/demo.tsx
@@ -9,7 +9,10 @@ export default function Demo() {
   return (
     <div>
       <br />
-      <myPlugin.Button>Click Me</myPlugin.Button>
+      <myPlugin.Button theme-color="danger">Click Me</myPlugin.Button>
+      <myPlugin.Button theme-text="success">Click Me</myPlugin.Button>
+      <myPlugin.Button theme-border="info">Click Me</myPlugin.Button>
+      <br />
       <br />
       {LOREM}
     </div>
